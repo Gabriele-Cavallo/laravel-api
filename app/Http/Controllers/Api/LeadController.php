03ac43fad54a6979ad7bb396aed1db8b7d98e2b7
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-06-13 16:47:25 +0200

    feat: add api LeadController route + LeadController store function + mail template

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Lead;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\NewContact;
+
+
+class LeadController extends Controller
+{
+    public function store(Request $request) {
+        $data = $request->all();
+
+        // Valido i dati
+        $validator = Validator::make($data, [
+            'name' => 'required|max:255',
+            'email' => 'required|email',
+            'message' => 'required'
+        ]);
+
+        // Se ci sono errori torno la risposta di errore
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        // Altrimenti salvo nel db il lead
+        $newLead = new Lead();
+        $newLead->fill($data);
+        $newLead->save();
+
+        // Invio la mail
+        Mail::to('admin@boolpress.com')->send(new NewContact($newLead));
+
+        // Dopo aver salvato i dati correttamente torno il messaggio di successo
+        return response()->json([
+            'success' => true
+        ]);
+    }
+}
---
+++ resources/views/emails/new-contact-mail.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Ciao Amministratore</h1>
+
+    <p>
+        Hai ricevuto un nuovo messaggio. <br>
+        Nome: {{ $lead->name }} <br>
+        Email: {{ $lead->mail }} <br>
+        Messaggio: <br> {{ $lead->message }}
+    </p>
+</body>
+</html>
